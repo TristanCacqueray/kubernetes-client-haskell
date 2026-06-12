89f3c0f2ca115c074e6d947ee3118c13a6005233
Tristan Cacqueray <tdecacqu@redhat.com>
2026-06-12 12:25:49 +0000

    Update for latest hoauth2

--- kubernetes-client/src/Kubernetes/Client/Auth/OIDC.hs
+++ kubernetes-client/src/Kubernetes/Client/Auth/OIDC.hs
@@ -25,7 +25,7 @@ import Kubernetes.Client.KubeConfig
 import Kubernetes.OpenAPI.Core
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
-import Network.OAuth.OAuth2                  as OAuth
+import Network.OAuth2                        as OAuth
 import Network.TLS                           as TLS
 import URI.ByteString
 import Web.OIDC.Client.Discovery             as OIDC
